1e7ccc970bbee3d5f5d1eb6195ef1435d6c159dc
Travis Gandy <travis_gandy@yahoo.com>
2020-04-30 19:46:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,16 @@
 # Add your code here
 class Dog 
   @@all = []
+  attr_reader :name 
   
+  def initialize(name)
+      @name = name 
+      @@all << self 
+  end 
   
+  def self.all
+      @@all 
+  end 
   
   
   
